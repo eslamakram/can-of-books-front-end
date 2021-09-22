c2728c060f72d5f714b74c8e52312c0828f73748
Eslam Akram <eslam_akram91@yahoo.com>
2021-09-22 21:18:34 +0300

    auth0

---
+++ src/components/LoginButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class LoginButton extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default LoginButton
