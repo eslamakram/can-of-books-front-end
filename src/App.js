23254ea334b9b446f3bcb383f7e977789c17f59a
Eslam Akram <eslam_akram91@yahoo.com>
2021-09-22 03:48:02 +0300

    finished all

--- src/App.js
+++ src/App.js
@@ -5,67 +5,153 @@ import axios from 'axios';
 import BookFormModal from './components/BookFormModal';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      data:[]
+    this.state = {
+      booksList: [],
+      title: '',
+      description: '',
+      status: '',
+      email: '',
+      id: '',
+      showUpdate: false
     }
   }
-  componentDidMount=()=>{
-    axios.get(`${process.env.REACT_APP_BACKEND_URL}/get-books`).then(response=>{
+  componentDidMount = () => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/get-books`).then(response => {
       this.setState({
-        data:response.data
+        booksList: response.data
       })
     })
   }
 
+ 
 
-  handleBookInput=e=>{
-    
-    this.setState({
-      title:e.target.value
-    })
-   
+  handleDeleteBook = (id) => {
+    let config = {
+      method: "DELETE",
+      baseURL: `${process.env.REACT_APP_BACKEND_URL}`,
+      url: `/delete-book/${id}`
+        } 
+        console.log(config);
+    axios(config).then(res => {
+      console.log(res.data)
+    });
   }
 
-  handleSubmit=e=>{
-    
-      e.preventDefault();
-      let config={
-        method:"POST",
-        baseURL:"http://localhost:8000",
-        url:"/create-book",
-        data:{
-              title: this.state.title,
-              description: this.state.description,
-              status: this.state.status,
-            email: this.state.email
-        }
-        
+  handleUpdateBook=(id,title,description,status,email)=>{
+    this.setState({
+      title: title,
+      description: description,
+      status: status,
+      email: email,
+      id:id,
+      showUpdate:true
+    })
+  }
+
+  handleUpdateForm=() =>{
+    let config={
+      method:"PUT",
+      baseURL:process.env.REACT_APP_BACKEND_URL,
+      url:`/update-book/${this.state.id}`,
+      data:{
+        title:this.state.title,
+        description:this.state.description,
+        status:this.state.status,
+        email:this.state.email
       }
-      axios(config).then(response=>{
+    }
+    axios(config).then(res=>{
+      this.setState({
+        booksList:res.data
+      })
+    });
+
+  }
+
+  handleBookTitle = e => { this.setState({ title: e.target.value }) }
+
+  handleBookDescripion = e => { this.setState({ description: e.target.value }) }
+
+  handleBookStatus = e => { this.setState({ status: e.target.value }) }
+
+  handleEmail = e => { this.setState({ email: e.target.value }) }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    let config = {
+      method: "POST",
+      baseURL: `${process.env.REACT_APP_BACKEND_URL}`,
+      url: "/create-book",
+      data: {
+        title: this.state.title,
+        description: this.state.description,
+        status: this.state.status,
+        email: this.state.email
+      }
+    }
+    if (this.state.title && this.state.description && this.state.status && this.state.email) {
+      axios(config).then(response => {
         this.setState({
-          booksList:response.data
+          booksList: response.data
         })
       })
-    
+    }
+
   }
 
-  render(){
-  return (
-    <>
-    
-    <BookFormModal handleSubmit={this.handleSubmit} handleBookInput={this.handleBookInput}/>
+  render() {
+    return (
+      <>
 
-<h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-{this.state.data.length ?  <BestBooks  booksData={this.state.data}  />
- :   <h3>No Books Found  </h3>}
+{
+          !this.state.showUpdate?<>
+           <BookFormModal
+          handleBookTitle={this.handleBookTitle}
+          handleBookDescripion={this.handleBookDescripion}
+          handleBookStatus={this.handleBookStatus}
+          handleEmail={this.handleEmail}
+          handleSubmit={this.handleSubmit} 
+          btnName={'Add Book'}/>
+                  
+          </>:
+         <BookFormModal
+          handleBookTitle={this.handleBookTitle}
+          valueTitle={this.state.title}
+          handleBookDescripion={this.handleBookDescripion}
+          valueDescription={this.state.description}
+          handleBookStatus={this.handleBookStatus}
+          valueStatus={this.state.status}
+          handleEmail={this.handleEmail}
+          valueEmail={this.state.email}
+          handleSubmit={this.handleUpdateForm}
+          btnName={'Update Book'} />
+       
+        }
 
-   
-    </>
-   
-  )
+
+        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+
+        {this.state.booksList.length ?
+          (this.state.booksList.map((book) => {
+            return <BestBooks
+              title={book.title}
+              description={book.description}
+              status={book.status}
+              email={book.email}
+              id={book._id}
+              handleDeleteBook={this.handleDeleteBook}
+              handleUpdateBook={this.handleUpdateBook}
+            />
+          }))
+          : (<h3>No Books Found  </h3>)}
+
+
+      </>
+
+    )
   }
 }
 
--- src/components/BookFormModal.js
+++ src/components/BookFormModal.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react'
-import { Form , Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button } from 'react-bootstrap'
 
 class BookFormModal extends Component {
     render() {
         return (
             <div>
-                <Form onClick={this.props.handleSubmit}>
+                <Form onClick={this.props.handleSubmit} >
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>ADD BOOK</Form.Label>
-                        <Form.Control type="text" placeholder="Book Title"  onChange={this.props.handleBookInput}/>
-                       </Form.Group>
+                        <Form.Label>BOOK Title</Form.Label>
+                        <input type="text" placeholder="Book Title" value={this.props.valueTitle} onChange={this.props.handleBookTitle} />
+                    </Form.Group>
 
                     <Form.Group className="mb-3" >
                         <Form.Label>Describe Book</Form.Label>
-                        <Form.Control type="text" placeholder="enter breif overview" />
+                        <input type="text" placeholder="enter breif overview" value={this.props.valueDescription}  onChange={this.props.handleBookDescripion} />
                     </Form.Group>
                     <Form.Group className="mb-3" >
                         <Form.Label>Book Status</Form.Label>
-                        <Form.Control type="text" placeholder="enter status book" />
+                        <input type="text" placeholder="enter status book" value={this.props.valueStatus}  onChange={this.props.handleBookStatus} />
                     </Form.Group>
                     <Form.Group className="mb-3" >
                         <Form.Label>email</Form.Label>
-                        <Form.Control type="email" placeholder="enter your email" />
+                        <input type="email" placeholder="enter your email" value={this.props.valueEmail} onChange={this.props.handleEmail} />
                     </Form.Group>
                     <Button variant="primary" type="submit">
-                        Add Book
+                         {this.props.btnName}
                     </Button>
                 </Form>
 
